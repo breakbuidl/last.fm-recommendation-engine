36f3d0225d03b58fbf3968ce34f5e1886aa1bf9c
psolanki10 <solankipinank10@gmail.com>
2019-06-28 14:22:43 +0530

    Added User model validations with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase!}
+  validates :name, presence: true, length: {maximum: 50}
+
+  #This helper validates that the attribute's value is unique right before the object gets saved.
+  #It does not create a uniqueness constraint in the database, so it may happen that two different database connections
+  #create two records with the same value for a column that you intend to be unique.
+  #To avoid that, you must create a unique index on that column in your database.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  # from rails docs
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+
+  # api.rubyonrails.org - If password confirmation validation is not needed, simply leave out the value
+  # for password_confirmation (i.e. don't provide a form field for it). When this attribute has a nil value,
+  # the validation will not be triggered.
   has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  describe "User attributes validations" do
+    let!(:user) { User.new(name: "John Doe", email:"sample@example.com", password:"some_password",
+                          password_confirmation:"some_password")}
+
+    context "Presence Validations" do
+      it "valid with valid attributes" do
+        expect(user).to be_valid
+      end
+
+      it "not valid without name" do
+        user.name = nil
+        expect(user).to_not be_valid
+      end
+
+      it "not valid without email" do
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+
+      it "not valid without password" do
+        user.password = nil                    #Can do this. password/password_confirmation source code - setter/getter
+        expect(user).to_not be_valid
+      end
+
+      it "not valid without password confirmation" do
+        user.password_confirmation = nil                    #Can do this. password/password_confirmation source code - setter/getter
+        expect(user).to_not be_valid
+      end
+    end
+
+    context "Length Validations" do
+      it "Name must be <= 50 chars" do
+        user.name = "a" * 51
+        expect(user).to_not be_valid
+      end
+
+      it "Password must be >= 6 chars" do
+        user.password = user.password_confirmation = "a" * 5
+        expect(user).to_not be_valid
+      end
+    end
+
+    context "Email format" do
+      it "Length must be <= 255 chars" do
+        user.email = "a" * 244 + "@example.com"
+        expect(user).to_not be_valid
+      end
+
+      it "Reject invalid addresses" do
+        #this list is not exhaustive
+        invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+
+        invalid_addresses.each do |invalid_address|
+          user.email = invalid_address
+          expect(user).to_not be_valid
+        end
+      end
+
+      it "Should be unique case insensitive" do
+        dup_user = user.clone
+        dup_user.email = user.email.upcase
+        user.save
+        expect(dup_user).to_not be_valid
+      end
+
+      it "Should be saved as lower-case" do
+        mixed_case_email = "Foo@Example.com"
+        user.email = mixed_case_email
+        user.save
+        expect(user.reload.email).to eq(mixed_case_email.downcase)
+      end
+    end
+  end
 end
