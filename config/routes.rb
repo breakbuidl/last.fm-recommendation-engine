f2e5a7dbd34307db3b7d4747807ca172317fd6ea
psolanki10 <solankipinank10@gmail.com>
2019-06-30 03:08:50 +0530

    Generated Sessions controller with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
-  get  '/signup',  to: 'users#new'
 end
