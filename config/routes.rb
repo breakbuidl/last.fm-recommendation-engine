258bb6336b62ac3d9180fb59f58fd51f7aec2441
psolanki10 <solankipinank10@gmail.com>
2019-07-01 21:49:59 +0530

    Customized layout with header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get    '/contact', to: 'home#contact'
+  get    '/about',   to: 'home#about'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
